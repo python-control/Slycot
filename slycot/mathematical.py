04325ef2cea51de93ded7b2c6e1d5973fa5b229c
Rene van Paassen <rene.vanpaassen@gmail.com>
2013-12-18 18:50:16 +0100

    work in progress

--- slycot/mathematical.py
+++ slycot/mathematical.py
@@ -23,124 +23,79 @@
 
 from slycot import _wrapper
 
-def mb05MD(BALANC, N, DELTA, A, LDA, V, LDV, Y, LDY, VALR,
-                        VALI, IWORK, DWORK, LDWORK, INFO ):
-    """
+def mb05md(n, A, delta, balanc='N')
+    """Ar, Vr, Yr, VALRr, VALDr = mb05md(n, a, delta, balanc='N')
+
     Matrix exponential for a real non-defective matrix
 
-     To compute exp(A*delta) where A is a real N-by-N non-defective
-     matrix with real or complex eigenvalues and delta is a scalar
-     value. The routine also returns the eigenvalues and eigenvectors
-     of A as well as (if all eigenvalues are real) the matrix product
-     exp(Lambda*delta) times the inverse of the eigenvector matrix
-     of A, where Lambda is the diagonal matrix of eigenvalues.
-     Optionally, the routine computes a balancing transformation to
-     improve the conditioning of the eigenvalues and eigenvectors.
+    To compute exp(A*delta) where A is a real N-by-N non-defective
+    matrix with real or complex eigenvalues and delta is a scalar
+    value. The routine also returns the eigenvalues and eigenvectors
+    of A as well as (if all eigenvalues are real) the matrix product
+    exp(Lambda*delta) times the inverse of the eigenvector matrix of
+    A, where Lambda is the diagonal matrix of eigenvalues.
+    Optionally, the routine computes a balancing transformation to
+    improve the conditioning of the eigenvalues and eigenvectors.
 
-     ARGUMENTS
-
-     Mode Parameters
-
-     BALANC  CHARACTER*1
-             Indicates how the input matrix should be diagonally scaled
+    Required arguments:
+        n : input int
+            The order of the matrix a.  n >= 0.
+        A : input rank-2 array('d') with bounds (n,n)
+            Square matrix
+        delta : input 'd'
+            The scalar value delta of the problem.
+     
+    Optional arguments:
+        balanc : input char*1
+           Indicates how the input matrix should be diagonally scaled
              to improve the conditioning of its eigenvalues as follows:
              = 'N':  Do not diagonally scale;
              = 'S':  Diagonally scale the matrix, i.e. replace A by
                      D*A*D**(-1), where D is a diagonal matrix chosen
                      to make the rows and columns of A more equal in
-                     norm. Do not permute.
+           The order of the matrix A.  N >= 0.
 
-     Input/Output Parameters
-
-     N       (input) INTEGER
-             The order of the matrix A.  N >= 0.
-
-     DELTA   (input) DOUBLE PRECISION
-             The scalar value delta of the problem.
-
-     A       (input/output) DOUBLE PRECISION array, dimension (LDA,N)
-             On entry, the leading N-by-N part of this array must
-             contain the matrix A of the problem.
-             On exit, the leading N-by-N part of this array contains
-             the solution matrix exp(A*delta).
-
-     LDA     INTEGER
-             The leading dimension of array A.  LDA >= max(1,N).
-
-     V       (output) DOUBLE PRECISION array, dimension (LDV,N)
-             The leading N-by-N part of this array contains the
-             eigenvector matrix for A.
-             If the k-th eigenvalue is real the k-th column of the
-             eigenvector matrix holds the eigenvector corresponding
-             to the k-th eigenvalue.
-             Otherwise, the k-th and (k+1)-th eigenvalues form a
-             complex conjugate pair and the k-th and (k+1)-th columns
-             of the eigenvector matrix hold the real and imaginary
-             parts of the eigenvectors corresponding to these
-             eigenvalues as follows.
-             If p and q denote the k-th and (k+1)-th columns of the
-             eigenvector matrix, respectively, then the eigenvector
-             corresponding to the complex eigenvalue with positive
-             (negative) imaginary value is given by
+    Return objects:
+        Ar : output rank-2 array('d') with bounds (n,n)
+           Contains the solution matrix exp(A*delta)
+        Vr : output rank-2 array('d') with bounds (n,n)
+           Contains the eigenvector matrix for A.  If the k-th
+           eigenvalue is real the k-th column of the eigenvector
+           matrix holds the eigenvector corresponding to the k-th
+           eigenvalue.  Otherwise, the k-th and (k+1)-th eigenvalues
+           form a complex conjugate pair and the k-th and (k+1)-th
+           columns of the eigenvector matrix hold the real and
+           imaginary parts of the eigenvectors corresponding to these
+           eigenvalues as follows.  If p and q denote the k-th and
+           (k+1)-th columns of the eigenvector matrix, respectively,
+           then the eigenvector corresponding to the complex
+           eigenvalue with positive (negative) imaginary value is
+           given by 
                                        2
-             p + q*j (p - q*j), where j  = -1.
+              p + q*j (p - q*j), where j  = -1.
+        Yr : output rank-2 array('d') with bounds (n,n)
+           contains an intermediate result for computing the matrix
+           exponential.  Specifically, exp(A*delta) is obtained as the
+           product V*Y, where V is the matrix stored in the leading
+           N-by-N part of the array V. If all eigenvalues of A are
+           real, then the leading N-by-N part of this array contains
+           the matrix product exp(Lambda*delta) times the inverse of
+           the (right) eigenvector matrix of A, where Lambda is the
+           diagonal matrix of eigenvalues.
 
-     LDV     INTEGER
-             The leading dimension of array V.  LDV >= max(1,N).
-
-     Y       (output) DOUBLE PRECISION array, dimension (LDY,N)
-             The leading N-by-N part of this array contains an
-             intermediate result for computing the matrix exponential.
-             Specifically, exp(A*delta) is obtained as the product V*Y,
-             where V is the matrix stored in the leading N-by-N part of
-             the array V. If all eigenvalues of A are real, then the
-             leading N-by-N part of this array contains the matrix
-             product exp(Lambda*delta) times the inverse of the (right)
-             eigenvector matrix of A, where Lambda is the diagonal
-             matrix of eigenvalues.
-
-     LDY     INTEGER
-             The leading dimension of array Y.  LDY >= max(1,N).
-
-     VALR    (output) DOUBLE PRECISION array, dimension (N)
-     VALI    (output) DOUBLE PRECISION array, dimension (N)
-             These arrays contain the real and imaginary parts,
-             respectively, of the eigenvalues of the matrix A. The
-             eigenvalues are unordered except that complex conjugate
-             pairs of values appear consecutively with the eigenvalue
-             having positive imaginary part first.
-
-     Workspace
-
-     IWORK   INTEGER array, dimension (N)
-
-     DWORK   DOUBLE PRECISION array, dimension (LDWORK)
-             On exit, if INFO = 0, DWORK(1) returns the optimal value
-             of LDWORK, and if N > 0, DWORK(2) returns the reciprocal
-             condition number of the triangular matrix used to obtain
-             the inverse of the eigenvector matrix.
-
-     LDWORK  INTEGER
-             The length of the array DWORK.  LDWORK >= max(1,4*N).
-             For good performance, LDWORK must generally be larger.
-
-     Error Indicator
-
-     INFO    INTEGER
-             = 0:  successful exit;
-             < 0:  if INFO = -i, the i-th argument had an illegal
-                   value;
-             = i:  if INFO = i, the QR algorithm failed to compute all
-                   the eigenvalues; no eigenvectors have been computed;
-                   elements i+1:N of VALR and VALI contain eigenvalues
-                   which have converged;
-             = N+1:  if the inverse of the eigenvector matrix could not
-                   be formed due to an attempt to divide by zero, i.e.,
-                   the eigenvector matrix is singular;
-             = N+2:  if the matrix A is defective, possibly due to
-                   rounding errors.
-
+        VALr : output rank-1 array('c') with bounds (n)
+           Contains the eigenvalues of the matrix A. The eigenvalues
+           are unordered except that complex conjugate pairs of values
+           appear consecutively with the eigenvalue having positive
+           imaginary part first.
     """
+    hidden = ' (hidden by the wrapper)'
+    arg_list = [ 'balanc', 'n', 'delta', 'a', 'lda'+hidden, 'v', 'ldv'+hidden,
+                 'y','ldy'+hidden,'valr','vali',
+                 'iwork'+hidden,'dwork'+hidden,'ldwork'+hidden,'info'+hidden]
+    out = _wrapper.mb05md(balanc=balanc,n=n,delta=delta,a=a,
+    
+
 
 
 def mb05nd(N, DELTA, A, LDA, EX, LDEX, EXINT, LDEXIN,
