5c005c2704331e5852d6d8d71ab0c962b2ee39b1
Shabroz Gill <shabroz.gill@gmail.com>
2016-07-20 13:27:03 +0200

    the 3 wrapped functions are made available in the module from the __init__.py file

--- slycot/__init__.py
+++ slycot/__init__.py
@@ -1,22 +1,23 @@
 import slycot.examples
 
 # Analysis routines (6/40 wrapped)
-from slycot.analysis import ab01nd,ab05md,ab05nd,ab07nd,ab08nd, ab09ad
+from slycot.analysis import ab01nd, ab05md, ab05nd, ab07nd, ab08nd, ab09ad
 
 # Data analysis routines (0/7 wrapped)
 
 # Filtering routines (0/6 wrapped)
 
-# Identification routines (0/5 wrapped)
+# Identification routines (3/5 wrapped)
+from slycot.identify import ib01ad, ib01bd, ib01cd
 
 # Mathematical routines (1/81 wrapped)
 from slycot.math import mc01td
 
 # Synthesis routines (11/50 wrapped)
-from slycot.synthesis import sb01bd,sb02md,sb02mt,sb02od,sb03md,sb04md,sb04qd,sb10ad,sb10hd,sg03ad,sg02ad
+from slycot.synthesis import sb01bd, sb02md, sb02mt, sb02od, sb03md, sb04md, sb04qd, sb10ad, sb10hd, sg03ad, sg02ad
 
 # Transformation routines (8/40 wrapped)
-from slycot.transform import tb01id,tb03ad, tb04ad
-from slycot.transform import tc04ad,tc01od
-from slycot.transform import tf01md,tf01rd
+from slycot.transform import tb01id, tb03ad, tb04ad
+from slycot.transform import tc04ad, tc01od
+from slycot.transform import tf01md, tf01rd
 from slycot.transform import td04ad
