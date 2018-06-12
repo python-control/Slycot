4d725ac374a6c4731ea3204398d05744e1f4571e
Rory Yorke <rory.yorke@gmail.com>
2018-06-12 21:51:25 +0200

    Fixes and workarounds for Conda build
    meta.yaml:
      - update to newer conda file layout
      - depend on openblas instead of lapack
    
    build.sh:
      - correct LDFLAGS setup
      - workaround how Conda setups up Fortran compilers

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,2 +1,20 @@
+#!/bin/bash
 cd $RECIPE_DIR/..
-LDFLAGS="-shared" FFLAGS="-fPIC" $PYTHON setup.py install
+
+# conda's activate-gcc_linux-64.sh sets LDFLAGS to have the directory
+# entries for finding libpython, liblapack, etc., so we can't
+# overwrite it
+export LDFLAGS="-shared $LDFLAGS"
+
+# conda's activate-gfortran_linux-64.sh sets F95, but
+# numpy.distutils.fcompiler looks for F90
+# this is hopefully a temporary hack
+if [ ! -v F90 ]; then
+    if [ ! -v F95 ]; then
+        echo "build.sh:error: neither F90 nor F95 variable set"
+        exit 1
+    fi
+    export F90="$F95"
+fi
+
+$PYTHON setup.py install
