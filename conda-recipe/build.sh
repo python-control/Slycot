0875919a81542ce9827404e9f98762c4025a662d
Rene van Paassen <Rene.vanPaassen@gmail.com>
2018-08-14 23:36:19 +0200

    hack a pip install for scikit-build

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,3 +1,5 @@
 cd $RECIPE_DIR/..
-export LAPACKLIBS=lapack:blas
+# until scikit-build is updated to 0.7.1 or higher ...
+$PYTHON -m pip install \
+	https://github.com/scikit-build/scikit-build/archive/0.7.1.zip
 $PYTHON setup.py install
