b3168f477708cc5a65af185c5294f4d924f7d03e
Rene van Paassen <m.m.vanpaassen@tudelft.nl>
2018-08-21 14:17:29 +0200

    Using scikit-build as buildsystem

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,3 +1,7 @@
 cd $RECIPE_DIR/..
-export LAPACKLIBS=lapack:blas
+# HACK, until scikit-build is updated to 0.7.1 or higher ...
+$PYTHON -m pip install \
+	https://github.com/scikit-build/scikit-build/archive/0.7.1.zip
 $PYTHON setup.py install
+# same HACK
+$PYTHON -m pip uninstall --yes scikit-build
