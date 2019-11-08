1cc11a484a83d3484b02db4022e671d068eb4552
Benjamin Greiner <greiner@dsi.uni-stuttgart.de>
2019-11-08 14:01:59 +0100

    move td04ad static test

--- slycot/tests/test.py
+++ slycot/tests/test.py
@@ -43,20 +43,6 @@ class Test(unittest.TestCase):
         self.assertAlmostEqual(Ac[1][0], 1)
         self.assertAlmostEqual(Ac[1][1], -3)
 
-    def test_td04ad_static(self):
-        """Regression: td04ad (TFM -> SS transformation) for static TFM"""
-        import numpy as np
-        from itertools import product
-        # 'C' fails on static TFs
-        for nout,nin,rc in product(range(1,6),range(1,6),['R']):
-            num = np.reshape(np.arange(nout*nin),(nout,nin,1))
-            if rc == 'R':
-                den = np.reshape(np.arange(1,1+nout),(nout,1))
-            else:
-                den = np.reshape(np.arange(1,1+nin),(nin,1))
-            index = np.tile([0],den.shape[0])
-            nr,a,b,c,d = transform.td04ad(rc,nin,nout,index,den,num)
-
 
 if __name__ == "__main__":
     unittest.main()
