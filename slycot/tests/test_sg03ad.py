d173984cd0f38aaf6017e8fea8cdf90db60489b2
Benjamin Greiner <greiner@dsi.uni-stuttgart.de>
2019-11-08 14:24:55 +0100

    remove unused module, clean pep8 warning

--- slycot/tests/test_sg03ad.py
+++ slycot/tests/test_sg03ad.py
@@ -8,24 +8,25 @@ import unittest
 from slycot import synthesis
 import numpy as np
 
-from numpy.testing import assert_raises, assert_almost_equal
+from numpy.testing import assert_almost_equal
 
 # test cases from
 # http://www.qucosa.de/fileadmin/data/qucosa/documents/4168/data/b002.pdf
+
 
 class test_sg03ad(unittest.TestCase):
 
     def test_sg03ad_a(self):
         # Example 1
         n = 100
-        Xref = np.ones((n,n))
+        Xref = np.ones((n, n))
         U = np.tril(Xref)
         for t in range(0, 50, 10):
-            A = 2.0**(-t) - np.eye(n) + np.diag(np.arange(1,n+1)) + U.T
+            A = 2.0**(-t) - np.eye(n) + np.diag(np.arange(1, n+1)) + U.T
             E = np.eye(n) + 2.0**(-t)*U
             Y = A.T.dot(Xref).dot(E) + E.T.dot(Xref).dot(A)
-            Q = np.zeros((n,n))
-            Z = np.zeros((n,n))
+            Q = np.zeros((n, n))
+            Z = np.zeros((n, n))
             A, E, Q, Z, X, scale, sep, ferr, alphar, alphai, beta = \
                 synthesis.sg03ad('C', 'B', 'N', 'N', 'L', n, A, E, Q, Z, Y)
             assert_almost_equal(Xref, X)
@@ -36,23 +37,20 @@ class test_sg03ad(unittest.TestCase):
                       [1.0, 3.0, 0.0],
                       [1.0, 0.0, 2.0]])
         E = np.array([[1.0, 3.0, 0.0],
-                       [3.0, 2.0, 1.0],
-                       [1.0, 0.0, 1.0]])
+                      [3.0, 2.0, 1.0],
+                      [1.0, 0.0, 1.0]])
         Y = np.array([[64.0, 73.0, 28.0],
-                       [73.0, 70.0, 25.0],
-                       [28.0, 25.0, 18.0]])
+                      [73.0, 70.0, 25.0],
+                      [28.0, 25.0, 18.0]])
         Xref = np.array([[-2.0000, -1.0000, 0.0000],
-                          [-1.0000, -3.0000, -1.0000],
-                          [0.0000, -1.0000, -3.0000]])
-        Q = np.zeros((3,3))
-        Z = np.zeros((3,3))
+                         [-1.0000, -3.0000, -1.0000],
+                         [0.0000, -1.0000, -3.0000]])
+        Q = np.zeros((3, 3))
+        Z = np.zeros((3, 3))
         A, E, Q, Z, X, scale, sep, ferr, alphar, alphai, beta = \
             synthesis.sg03ad('C', 'B', 'N', 'N', 'L', n, A, E, Q, Z, -Y)
-        #print(A, E, Q, Z, X, scale, sep)
+        # print(A, E, Q, Z, X, scale, sep)
         assert_almost_equal(X, Xref)
-
-def suite():
-   return unittest.TestLoader().loadTestsFromTestCase(TestConvert)
 
 
 if __name__ == "__main__":
