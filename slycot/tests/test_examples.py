36ceb478b1a88fdb00eae9eca036716a7bb057f5
Benjamin Greiner <code@bnavigator.de>
2020-05-24 18:28:33 +0200

    make test_example more verbose on failures

--- slycot/tests/test_examples.py
+++ slycot/tests/test_examples.py
@@ -14,7 +14,10 @@ examplefunctions = [fun for (fname, fun) in getmembers(examples)
                     if isfunction(fun) and "_example" in fname]
 
 
+#ignore numpy ABI changes https://github.com/numpy/numpy/pull/432
 @pytest.mark.parametrize('examplefun', examplefunctions)
+@pytest.mark.filterwarnings("ignore:numpy.dtype size changed")
+@pytest.mark.filterwarnings("ignore:numpy.ufunc size changed")
 def test_example(examplefun, capsys, recwarn):
     """
     Test the examples.
@@ -24,6 +27,18 @@ def test_example(examplefun, capsys, recwarn):
     """
     examplefun()
     captured = capsys.readouterr()
-    assert len(captured.out) > 0
-    assert not captured.err
-    assert not recwarn
+
+    # fail for first in order
+    failconditions = [
+        ((not len(captured.out) > 0), "Example {} did not print any results\n"),
+        (captured.err, "Example {} wrote to stderr\n"),
+        (recwarn,  "Example {} produced a warning.\n")]
+    for failed, msgfmt  in failconditions:
+        if failed:
+            pytest.fail(msgfmt.format(examplefun.__name__) +
+                        "Captured output:\n{}\n"
+                        "Captured stderr:\n{}\n"
+                        "Captured warnings:\n{}\n"
+                        "".format(captured.out,
+                                  captured.err,
+                                  [w.message for w in recwarn]))
