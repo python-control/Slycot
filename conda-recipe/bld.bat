f4bb9eb5d5195cf31940813985264239f4ecdfdb
Rory Yorke <rory.yorke@gmail.com>
2026-06-15 06:34:15 +0200

    Rename CMakeLists.txt var. SLYCOT_CI to SLYCOT_WINDOWS_CONDA_BUILD

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,8 +1,9 @@
 @echo on
 
-set "SKBUILD_CMAKE_ARGS=-GNinja;-DVERBOSE=ON;-DSLYCOT_CI=ON"
-
-echo SKBUILD_CMAKE_ARGS is - %SKBUILD_CMAKE_ARGS% -
+@rem -GNinja selects Ninja build generator
+@rem SLYCOT_WINDOWS_CONDA_BUILD clears a debug build setting that
+@rem otherwise prevents the build
+set "SKBUILD_CMAKE_ARGS=-GNinja;-DSLYCOT_WINDOWS_CONDA_BUILD=ON"
 
 %PYTHON% -m pip install --no-deps --no-build-isolation -vv .
 if %ERRORLEVEL% neq 0 exit 1
